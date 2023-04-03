440f5110c6725d52af50f658e1bc61dcb8c07729
Gui089 <guilhermesilba811@gmail.com>
2023-04-03 16:19:44 -0300

    Primeira versao

---
+++ Applicatioin/Program.java
@@ -0,0 +1,15 @@
+package Applicatioin;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessMatch;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.gtePieces());
+    }
+
+}
---
+++ chess/ChessExcepetion.java
@@ -0,0 +1,9 @@
+package chess;
+
+public class ChessExcepetion extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public ChessExcepetion(String msg) {
+        super(msg);
+    }
+}
